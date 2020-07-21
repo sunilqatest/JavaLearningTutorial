fe81ac4008f172c88fbbc400ef05e67f11aa4309
sunilqatest <68591965+sunilqatest@users.noreply.github.com>
2020-07-21 19:01:34 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.learn.java.string;
+
+public class HelloWorld {
+	int i = 10;
+	private int j = 10;
+	protected int k = 10;
+	static int l = 10; 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ksbgks");
+		
+
+	}
+
+}
